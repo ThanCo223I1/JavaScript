585b1a7687b938d704a0f51c6ef4b7acf0c96e11
than <user@users-MacBook-Pro.local>
2023-04-05 15:10:17 +0700

    BT

--- JavaScript/Sudungtoantu.js
+++ JavaScript/Sudungtoantu.js
@@ -1,8 +1,8 @@
 let inputWidth;
 let inputHeight;
-inputWidth=prompt("Nhap Do Dai: ");
-inputHeight = prompt("Nhap Do Rong: ");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
+inputWidth= +prompt("Nhap Do Dai: ");
+inputHeight = +prompt("Nhap Do Rong: ");
+let width = +parseInt(inputWidth);
+let height = +parseInt(inputHeight);
 let are = width * height;
 document.write("This is Are: " + are);
