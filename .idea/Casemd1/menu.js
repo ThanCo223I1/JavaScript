abf51bbceec72edd398d7cc09954b93cf0118a14
than <user@users-MacBook-Pro.local>
2023-04-25 08:19:39 +0700

    case

---
+++ .idea/Casemd1/menu.js
@@ -0,0 +1,17 @@
+
+    function login() {
+    window.document.location.href = "DangNhap.html"
+}
+
+    function register() {
+    window.document.location.href = "DangKiCuaHang.html"
+}
+    function now() {
+    window.document.location.href ="DatHang.html"
+}
+    function cart(){
+    window.document.location.href = "Thanh Toan.html"
+}
+ function editProduct() {
+ }
+
